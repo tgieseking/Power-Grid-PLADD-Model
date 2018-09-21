ba433df4be0760030e7f1b3aaa16447361170abc
Timothy Gieseking <tgiesekiing2.71@gmail.com>
2018-09-20 21:26:49 -0400

    Add function to directly calculate the probabilies we reach each output node of a Markov chain

--- PLADD/Markov.py
+++ PLADD/Markov.py
@@ -11,6 +11,36 @@ class Markov:
             current_node = current_node.next_node()
         return current_node.output
 
+    def calculate_output_probs(self):
+        # if this object is a Markov chain that is a DAG where the start node is the only chain with no inputs, this function calculates the probabilites of the chain getting each possible output
+
+        # all three of these function track probabilites that the Markov chain eventually is in a given node
+        output_probs = {}# all three of these function track probabilites that the Marko
+        # final probabilies of output nodes
+        in_progress_probs = {self.start_node:1}
+        # probabilies of nodes that we have not finished calculating yet
+        final_probs = {}
+        # probabilies of nodes that we are done calculating with
+
+        while(in_progress_probs):
+            for node in dict(in_progress_probs):
+                # we copy in_progress_probs here because we are modifying in_progress_probs inside the loop
+                if all([(prev in final_probs) for prev in node.previous_nodes]):
+                    # we have finished calculating the probabilies of all previous nodes, so this node is done
+                    if node.final:
+                        if node.output in output_probs:
+                            output_probs[node.output] += in_progress_probs.pop(node)
+                        else:
+                            output_probs[node.output] = in_progress_probs.pop(node)
+                    else:
+                        final_probs[node] = in_progress_probs.pop(node)
+                        for next_node, transition_prob in node.transitions:
+                            if next_node in in_progress_probs:
+                                in_progress_probs[next_node] += transition_prob * final_probs[node]
+                            else:
+                                in_progress_probs[next_node] = transition_prob * final_probs[node]
+        return output_probs
+
 class MarkovNode:
     # a single node of a Markov chain
     def __init__(self, final, output = None):
